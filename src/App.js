faf962ab7e4d174f33bc0d0d935695057d145ae9
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-01 21:14:40 -0300

    instalando e estilizando com sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/globalStyles.scss'
 import Home from './pages/Home';
 
 const App = () => {
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,15 @@
+import styles from  './Cabecalho.module.scss'
+
 const Header = () => {
     return(
-        <header>
+        <header className={styles.header}>
             <img src="/assets/logo.png" alt="Logo do Alura Space" />
-            <div>
-                <input type="text" placeholder="O que você procura?"/>
+            <div className={styles.header__container}>
+                <input 
+                    className={styles.header__input} 
+                    type="text" 
+                    placeholder="O que você procura?"
+                />
                 <img src="/assets/search.png" alt="Icone de lupa" />
             </div>
         </header>

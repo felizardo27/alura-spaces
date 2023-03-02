c2cd4d9c453a61ef8ca88a66f1ffe5a66bb034b8
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-01 21:47:06 -0300

    refatorando codigo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,19 @@
 import styles from  './Cabecalho.module.scss'
+import logo from '../../assets/logo.png'
+import search from '../../assets/search.png'
+
 
 const Header = () => {
     return(
         <header className={styles.header}>
-            <img src="/assets/logo.png" alt="Logo do Alura Space" />
+            <img src={logo} alt="Logo do Alura Space" />
             <div className={styles.header__container}>
                 <input 
                     className={styles.header__input} 
                     type="text" 
                     placeholder="O que você procura?"
                 />
-                <img src="/assets/search.png" alt="Icone de lupa" />
+                <img src={search} alt="Icone de lupa" />
             </div>
         </header>
     )

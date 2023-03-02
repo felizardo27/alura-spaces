b0e7f24403af9802c053c2f58250431311cc2e1a
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-01 22:00:18 -0300

    criando componente menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './Menu.module.scss'
+import home from '../../assets/icones/home-ativo.png'
+import views from '../../assets/icones/mais-vistas-inativo.png'
+import likes from '../../assets/icones/mais-curtidas-inativo.png'
+import news from '../../assets/icones/novas-inativo.png'
+import suprise from '../../assets/icones/surpreenda-me-inativo.png'
+
+const Menu = () => {
+  return (
+    <nav className={styles.menu}>
+        <ul className={styles.menu__lista}>
+            <li className={styles.menu__item}>
+                <img src={home} alt=''/>
+                <a href='/'>Inicio</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={views} alt=''/>
+                <a href='/'>Mais vistas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={likes} alt=''/>
+                <a href='/'>Mais curtidas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={news} alt=''/>
+                <a href='/'>Novas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={suprise} alt=''/>
+                <a href='/'>Surpreenda-me</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Menu
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,12 @@
 import Header from "../../components/Header"
+import Menu from "../../components/Menu"
 
 const Home = () => {
     return(
-        <Header />
+        <>
+            <Header />
+            <Menu />
+        </>
     )
 }
 

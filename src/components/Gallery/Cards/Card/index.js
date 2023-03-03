fbcd5a750f71b1b2314a4a915ad3aa77eb98c19e
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-03 19:39:53 -0300

    criando componente card

---
+++ src/components/Gallery/Cards/Card/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import favorito  from'../../../../assets/favorito.png'
+import open  from'../../../../assets/open.png'
+
+
+const Card = ({photo, styles}) => {
+  return (
+    <li key={photo.id} className={styles.gallery__card}>
+        <img 
+            className={styles.gallery__imagem} 
+            src={photo.imagem} 
+            alt={photo.titulo}
+        />
+        <p className={styles.gallery__descricao}>{photo.titulo}</p>
+        <div>
+            <p>{photo.creditos}</p>
+            <span>
+            <img src={favorito} alt='Icone de coração de curtir' />
+            <img src={open} alt='Icone de abir modal' />
+            </span>
+        </div>
+    </li>
+  )
+}
+
+export default Card
--- src/components/Gallery/Cards/index.js
+++ src/components/Gallery/Cards/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import favorito  from'../../../assets/favorito.png'
-import open  from'../../../assets/open.png'
+import Card from './Card'
+
 
 
 const Cards = ({photos, styles}) => {
@@ -8,21 +8,10 @@ const Cards = ({photos, styles}) => {
     <ul className={styles.gallery__cards}>
           {photos.map(photo => {
             return (
-              <li key={photo.id} className={styles.gallery__card}>
-                <img 
-                  className={styles.gallery__imagem} 
-                  src={photo.imagem} 
-                  alt={photo.titulo}
-                />
-                <p className={styles.gallery__descricao}>{photo.titulo}</p>
-                <div>
-                  <p>{photo.creditos}</p>
-                  <span>
-                    <img src={favorito} alt='Icone de coração de curtir' />
-                    <img src={open} alt='Icone de abir modal' />
-                  </span>
-                </div>
-              </li>
+              <Card 
+                photo={photo}
+                styles={styles}
+              />
             )
           })}
         </ul>

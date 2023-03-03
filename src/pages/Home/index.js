387a3e97e971c8df25786edafb044c52fa784a87
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-02 21:52:20 -0300

    adicionando variaveis e refatorando o código

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import Banner from "../../components/Banner"
 import Header from "../../components/Header"
 import Menu from "../../components/Menu"
-
 import styles from './Home.module.scss'
 
 const Home = () => {
